89fdf8b76d1994ebb1d34354c1bab3b69b83aca5
Haoran Li <haorli@gl-login1.arc-ts.umich.edu>
2024-11-22 18:29:37 -0500

    update_install

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Mini",                     # This is the name of the package
-    version="0.0.1",                        # The initial release version
+    name="miniQuant",                     # This is the name of the package
+    version="1.0",                        # The initial release version
     author="Haoran Li",                     # Full name of the author
-    description="Mini:a highly-accurate bioinformatics tool for transcript abundance estimation.",
+    description="M͟i͟xed Bayesian n̲etwork for i̲soform quantification (miniQuant) provides a highly-accurate bioinformatics tool for transcript abundance estimation.",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),    # List of all python modules to be installed
+    license_files = ('LICENSE',),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["Mini"],             # Name of the python package
+    python_requires='>=3.8',                # Minimum version requirement of the package
+    py_modules=["miniQuant"],             # Name of the python package
     package_dir={'':'isoform_quantification'},     # Directory of the source code of the package
     install_requires=required                    # Install other dependencies if any
 )
